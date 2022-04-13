0f29cf0374311a1de16a59298c8ffb5865436cc6
TK <ottertut@gmail.com>
2022-04-13 17:25:46 +0300

    message

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  head() {
+    return {
+      title: 'Event Listing',
+    }
+  },
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then((response) => {
+      return {
+        events: response.data,
+      }
+    })
+  },
+  components: {
+    EventCard,
+  },
+}
+</script>
